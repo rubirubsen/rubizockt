42f922410751ed45ff24f811e0ff1debc58ec720
rubirubsen <rubirubsen@gmail.com>
2023-09-02 16:15:42 +0200

    Aktueller Stand 9/23

--- data/navbar.php
+++ data/navbar.php
@@ -1,8 +1,24 @@
-<div class="navbar">
-    <a href="index.php">Home</a> 
-    | <a href="zocken.php">Rubi Zockt</a> 
-    | <a href="blog.php">Rubi Bloggt</a> 
-    | <a href="https://pure4u.de/impressum/" target="_blank">Impressum</a> 
-    | <a href="toolboard.php">Toolboard</a>
-    | <a href="apis.php">API's</a>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>RubiZockt bald wieder!</title>
+    <link rel="stylesheet" href="css/rubStyle.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
+</head>
+<body style="background:darkgrey">
+<div class="mainContent" id="mainContent">
+    <div class="logo" id="logo">
+        RubiZockt
+    </div>
+<!--<img src="img/header_raw.png" id="imgHead" name="imgHead" width="100%" style="margin-bottom: 0px;">-->
+<div class="navbar" id="navbar" name="navbar" style="margin-left: 0px">
+    <a href="index.php">Home</a>
+    | <a href="zocken.php">Rubi Zockt</a>
+    | <a href="blog.php">Rubi Bloggt</a>
+    | <a href="bot.php">Twitch-Bot</a>
+    | <a href="server.php">Rubis Server</a>
+    | <a href="https://pure4u.de/impressum/" target="_blank">Impressum</a>
 </div>
+    <div class="scrollContent" id="scrollContent" name="scrollContent">
