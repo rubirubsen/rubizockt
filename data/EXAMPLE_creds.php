694d1e6b1f6674108ae4d7ca0010866a6f9173bd
rubirubsen <rubirubsen@gmail.com>
2023-09-02 16:27:21 +0200

    Examplefile for creds.php

--- data/EXAMPLE_creds.php
+++ data/EXAMPLE_creds.php
@@ -1,60 +1,75 @@
 <?php
+//SQL-Daten
+$servername = "SQL_URL";
+$username = "SQL_USER";
+$password = "SQL_PASSWORD";
+$dbname = "DATABASE";
 
-$servername = "rubizockt.de";
-$username = "SA";
-$password = "K4ff33p0tt.";
-$dbname = "rzde";
-
-$tw_client_id = 'vnziuw3gqfu8axrbf90jzh4ng9q2cp';
-$tw_client_secret= 'of7wwcgtyk7ibntkw4swzw2w3qxqe1';
+//Twitch-Daten
+$tw_client_id = 'CLIENT_ID';
+$tw_client_secret= 'CLIENT_SECRET';
 
 $tw_bearer = getTwitchBearer($tw_client_id,$tw_client_secret);
 $streamArray = getDayZstreams($tw_bearer, $tw_client_id);
 
 
+/**
+ * @param $clientID string Twitch Client-ID
+ * @param $clientSecret string Twitch Client-Secret
+ * @return string Twitch Bearer-Token
+ */
 function getTwitchBearer($clientID, $clientSecret){
-    $url = "https://id.twitch.tv/oauth2/token";
-    $curl = curl_init($url);
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_POST, true);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $url = "https://id.twitch.tv/oauth2/token";
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-    $headers = array(
-        "Content-Type: application/x-www-form-urlencoded",
-    );
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        $headers = array(
+            "Content-Type: application/x-www-form-urlencoded",
+        );
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
-    $data = 'client_id='.$clientID.'&client_secret='.$clientSecret.'&grant_type=client_credentials';
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        $data = 'client_id='.$clientID.'&client_secret='.$clientSecret.'&grant_type=client_credentials';
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
 
-//for debug only!
-    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    //for debug only!
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
-    $resp = curl_exec($curl);
-    curl_close($curl);
-    $respDecode = json_decode($resp,true);
+        $resp = curl_exec($curl);
+        curl_close($curl);
+        $respDecode = json_decode($resp,true);
 
-    return $respDecode['access_token'];
-}
+        return $respDecode['access_token'];
+    }
+
+
+/**
+ * @param $tw_bearer string Twitch Bearer-Token
+ * @param $tw_client_id string Twitch Cliend ID
+ * @return bool|string Response from Twitch
+ */
 function getDayZstreams($tw_bearer, $tw_client_id){
-    $url = "https://api.twitch.tv/helix/streams?language=de&type=live&game_id=65632&first=100";
+        $url = "https://api.twitch.tv/helix/streams?language=de&type=live&game_id=65632&first=100";
 
-    $curl = curl_init($url);
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-    $headers = array(
-        "Authorization: Bearer ".$tw_bearer,
-        "Client-Id: ".$tw_client_id,
-    );
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-//for debug only!
-    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        $headers = array(
+            "Authorization: Bearer ".$tw_bearer,
+            "Client-Id: ".$tw_client_id,
+        );
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
-    $resp = curl_exec($curl);
-    curl_close($curl);
-    return $resp;
-}
+        //for debug only!
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+        $resp = curl_exec($curl);
+        curl_close($curl);
+
+        return $resp;
+    }
 ?>
